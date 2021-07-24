f4404dc3f5ea7c9049497986b8b52ba33ed1fd2c
Salih TANER <taner2003@gmail.com>
2021-07-24 11:12:38 +0200

    like and dislike answer

--- app/controllers/answerController.js
+++ app/controllers/answerController.js
@@ -93,6 +93,41 @@ module.exports = {
             success: true,
             message: "Asnwer deleted successfull"
         });
+    },
+    async likeAnswer(request, response, next) {
+        const { answer_id } = request.params;
+
+        const answer = await Answer.findById(answer_id);
+        
+        console.log(answer.likes);
+        if (answer.likes.includes(request.user.id)) {
+            return next(new CustomError("You already liked this answer", 400));
+        };
+
+        answer.likes.push(request.user.id);
+        await answer.save();
+
+        response.status(200).json({
+            success: true,
+            data: answer
+        });
+    },
+    async undoLikeAnswer(request, response, next) {
+        const { answer_id } = request.params;
+
+        const answer = await Answer.findById(id);
+        
+        if (!answer.likes.includes(request.user.id)) {
+            return next(new CustomError("You can not undo like operation for this answer", 400));
+        }
+        const index = answer.likes.indexOf(request.user.id);
+        answer.likes.splice(index, 1);
+        await answer.save();
+
+        response.status(200).json({
+            success: true,
+            data: answer
+        });
     }
     
 }
--- app/routers/answers.js
+++ app/routers/answers.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const { getAccessToRoute, getAnswerOwnerAccess } = require("../middlewares/authorization/auth");
-const { addNewAnswerToQuestion, getAllAnswersByQuestion, getSingleAnswer, editAnswer, deleteAnswer } = require("../controllers/answerController");
+const { 
+    addNewAnswerToQuestion,
+    getAllAnswersByQuestion,
+    getSingleAnswer,
+    editAnswer,
+    deleteAnswer,
+    likeAnswer,
+    undoLikeAnswer
+} = require("../controllers/answerController");
 const {catchErrors} = require('../helpers/catchError');
 const { checkQuestionAndAnswerExist } = require('../middlewares/database/databaseErrorHelpers');
 
@@ -9,6 +17,8 @@ const router = express.Router({mergeParams:true});
 router.post('/', getAccessToRoute,  catchErrors(addNewAnswerToQuestion));
 router.get('/', catchErrors(getAllAnswersByQuestion));
 router.get('/:answer_id', checkQuestionAndAnswerExist, catchErrors(getSingleAnswer));
+router.get('/:answer_id/like', [ checkQuestionAndAnswerExist,getAccessToRoute ], catchErrors(likeAnswer));
+router.get('/:answer_id/undo_like', [ checkQuestionAndAnswerExist,getAccessToRoute ], catchErrors(undoLikeAnswer));
 router.put('/:answer_id/edit', [ checkQuestionAndAnswerExist,getAccessToRoute,getAnswerOwnerAccess ], catchErrors(editAnswer));
 router.delete('/:answer_id/delete', [ checkQuestionAndAnswerExist,getAccessToRoute,getAnswerOwnerAccess ], catchErrors(deleteAnswer));
 
